0597f12636b4b90efd5f75464698f00d562fb16d
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2023-11-29 19:04:32 +0300

    almost

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,52 @@
-'use client';
+"use client";
 import styles from "./page.module.css";
 import Header from "./Components/Header/Header";
 import { Container } from "./Components/Container/Container";
 import { Card } from "./Components/Card/Card";
 import Footer from "./Components/Footer/Footer";
-import { useState ,useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-    const getData = () => {
-      fetch(`https://api.slingacademy.com/v1/sample-data/blog-posts`)
-        .then((res) => res.json())
-        .then((data) => {
-          setBlogs(data.blogs);
-          setLoading(false);
-        });
+  const getData = async (pageNumber) => {
+    try {
+      const res = await fetch(
+        `https://api.slingacademy.com/v1/sample-data/blog-posts?page=${pageNumber}`
+      );
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await res.json();
+      setBlogs((prevBlogs) => [...prevBlogs, ...data.blogs]);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    getData(page);
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
-    useEffect(() => {
-      getData();
-    }, []);
+  }, [page]);
+
   return (
     <div className={styles.home}>
       <Header />
@@ -35,7 +61,11 @@ export default function Home() {
         </div>
       </div>
       <Container>
-        {loading && <div className={styles.loading}> <span className={styles.loader}></span></div>}
+        {loading && (
+          <div className={styles.loading}>
+            <span className={styles.loader}></span>
+          </div>
+        )}
         <div className={styles.grid}>
           {blogs.map((blog, i) => (
             <Card key={i} blog={blog} />
