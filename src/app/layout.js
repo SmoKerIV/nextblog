e4766f4db1cf558855f115d782efe11a40cba3b7
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2023-12-05 01:42:17 +0300

    added top loader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={1}
+          crawlSpeed={500}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={1000}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
+        {children}
+      </body>
     </html>
   );
 }
